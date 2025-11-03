4d3beb1ef2dc655b7bdaea3f20bd3643706f8cc0
nessatuncay <144923042+nessatuncay@users.noreply.github.com>
2025-11-02 20:02:54 -0500

    added doctype, lang, charset, title and body header to the php file

---
+++ assignment-1/confirm.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Confirmed Orders</title>
+    </head>
+    <body>
+        <header>
+            <h1>Pizza Coven - Order Confirmations</h1>
+            <p>Thank you for ordering from Pizza Coven! Here are all of the confirmed pizza orders!</p>
+        </header>
+
+</html>
